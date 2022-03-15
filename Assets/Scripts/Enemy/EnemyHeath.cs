a8f9d1b6d65cca91c4ab73d7fa777589aa709017
Nguyen Sang <nguyenvansang30102001@gmail.com>
2022-03-15 16:09:39 +0700

    Add Heath Enemy

---
+++ Assets/Scripts/Enemy/EnemyHeath.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class EnemyHeath : MonoBehaviour
+{
+    //Máu max
+    public int MaxHeath;
+    //Máu hiện tại
+    int HealthCurrent;
+    //Tham chiếu thanh máu
+    public Slider HeathSlider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        HealthCurrent = MaxHeath;
+        HeathSlider.maxValue = MaxHeath;
+        HeathSlider.value = MaxHeath;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    //Bị tấn công
+    public void addDame(int Damage)
+    {
+        HealthCurrent -= Damage;
+        HeathSlider.value = HealthCurrent;
+        if(HealthCurrent<=0)
+        {
+            makeDead();
+        }    
+    }    
+    // Khi enemy chết
+    void makeDead()
+    {
+        Destroy(gameObject);
+    }    
+}
