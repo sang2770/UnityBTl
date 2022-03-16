a71acbe0c6c7735995a624036e2dfc9729d27360
Nguyen Sang <nguyenvansang30102001@gmail.com>
2022-03-16 09:19:02 +0700

    Fix Gay Dame quai

--- Assets/Scripts/DameController.cs
+++ Assets/Scripts/DameController.cs
@@ -28,11 +28,10 @@ public class DameController : MonoBehaviour
     }
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if(collision.gameObject.tag=="Enemy")
+        if(collision.gameObject.tag=="Enemy" && gameObject.tag=="Bullet")
         {
             
             Destroy(gameObject);
-
         }
     }
 }
