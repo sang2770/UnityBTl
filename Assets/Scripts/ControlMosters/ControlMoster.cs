703cb8a7581e781b434fe90da6116d175f597a2e
hung <hung71718@gmail.com>
2022-03-16 22:36:58 +0700

    update enemy

--- Assets/Scripts/ControlMosters/ControlMoster.cs
+++ Assets/Scripts/ControlMosters/ControlMoster.cs
@@ -7,6 +7,7 @@ public class ControlMoster : MonoBehaviour
     public float maxX;
     public float minX;
     public float speed = 2f;
+    public int HuongBanDau=0;
     Rigidbody2D rigidbody;
     private float Direction;
     // Start is called before the first frame update
@@ -19,14 +20,29 @@ public class ControlMoster : MonoBehaviour
     // Update is called once per frame
     private void FixedUpdate()
     {
+        Vector3 scale= transform.localScale;
         if (transform.position.x >= maxX)
         {
             Direction = -transform.localScale.y;
+            scale.x = -1;
+            if (HuongBanDau == 1)
+            {
+                scale.x = 1;
+            }
+
+
         }
         else if(transform.position.x <= minX) 
             {
                 Direction = transform.localScale.y;
-            } 
+            scale.x = 1;
+            if (HuongBanDau == 1)
+            {
+                scale.x = -1;
+            }
+        } 
         rigidbody.velocity = new Vector2(Direction*speed,rigidbody.velocity.y);
+        
+        transform.localScale = scale;
     }
 }
