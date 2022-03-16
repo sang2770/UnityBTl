11115cf1fdb6eecabcaead4f45db0cf808264f35
Son <hoangson7423@gmail.com>
2022-03-16 22:32:27 +0700

    sonup

---
+++ Assets/Menucontroller.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menucontroller : MonoBehaviour
+{
+    public void Playgame()
+    {
+        Application.LoadLevel("Scenes 1");
+    }
+}
